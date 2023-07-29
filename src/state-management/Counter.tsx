40e148b5a70c12df2aa041fb426b4ce0a0337b1d
Ali Mohammadinia <alimohammadinia3141@gmail.com>
2023-07-29 14:57:03 +0330

    Consolidating state logic with a reducer

---
+++ src/state-management/Counter.tsx
@@ -0,0 +1,25 @@
+import {useReducer} from "react";
+import counterReducer from "./reducers/counterReducer.ts";
+
+const Counter = () => {
+    const [value, dispatch] = useReducer(counterReducer, 0);
+    return (
+        <div>
+            Counter ({value})
+            <button
+                onClick={() => dispatch({ type: 'INCREMENT' })}
+                className="btn btn-primary mx-1"
+                >
+                Increment
+            </button>
+            <button
+                onClick={() => dispatch({ type: 'RESET' })}
+                className="btn btn-primary mx-1"
+            >
+                Reset
+            </button>
+        </div>
+    );
+};
+
+export default Counter;
