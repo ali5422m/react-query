fed40e048b266ba779ce287736e683becdd760f4
Ali Mohammadinia <alimohammadinia3141@gmail.com>
2023-08-01 00:51:37 +0330

    useParams && useSearchParams && useLocation

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,8 +1,19 @@
+import {useLocation, useParams, useSearchParams} from "react-router-dom";
 
 const UserDetailPage = () => {
+    const params = useParams();
+    console.log(params);
+
+    const [searchParams, setSearchParams] = useSearchParams();
+    console.log(searchParams.toString());
+    console.log(searchParams.get("name"));
+
+    const location = useLocation();
+    console.log(location);
+
     return (
         <div>
-            salam
+            User
         </div>
     );
 };
