ae3b72fd841a4a0e7b2a3722161fa66927ae1a7f
Ali Mohammadinia <alimohammadinia3141@gmail.com>
2023-08-03 00:45:47 +0330

    private routes - useAuth && LoginPage

--- src/routing/hooks/useAuth.ts
+++ src/routing/hooks/useAuth.ts
@@ -1,6 +1,6 @@
 
-const useAuth = () => ({ user: {id:1, name: "Ali" }});
+// const useAuth = () => ({ user: {id:1, name: "Ali" }});
 
-// const useAuth = () => ({ user: null});
+const useAuth = () => ({ user: null});
 
 export default useAuth;
